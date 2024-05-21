cf1a95edda6d1fdf5de83a8031f0b709252b6cbb
drevelob <140353802+drevelob@users.noreply.github.com>
2024-05-21 16:32:17 -0500

    Add event listener to digits
    Write function setDigit

--- script.js
+++ script.js
@@ -11,3 +11,23 @@ const equalBtn = document.querySelector('.equal');
 let numberA = '';
 let numberB = '';
 let operator = '';
+
+digitsBtn.forEach((dig) => {
+  dig.addEventListener('click', setDigit);
+});
+
+function setDigit() {
+  const digitTarget = this.value;
+
+  if (operator) {
+    if (numberB !== '0') {
+      numberB += digitTarget;
+      display.textContent += digitTarget;
+    }
+  } else {
+    numberA = display.textContent === '0'
+      ? digitTarget
+      : numberA += digitTarget;
+    display.textContent = numberA;
+  }
+}
